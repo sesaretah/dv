2c8b8e2f011ea79dbd2966aded9579fa73dfd37a
Hossein Shafiei <h.shafiei@ut.ac.ir>
2018-11-04 19:18:05 +0330

    1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,30 @@ module ApplicationHelper
     @options = ['has','has_not']
     return @options[s-1]
   end
+
+  def permit_by_workflow(article,user,caller)
+    if article.workflow_state.blank?
+      return true
+    end
+    @role = Role.find_by_id(user.current_role_id)
+    if !@role.blank?
+      if article.workflow_state.role_id != @role.id
+        return false
+      end
+    end
+    @items = article.workflow_state.editable.split(',')
+    @flag = false
+    for item in @items
+      if !item.blank?
+        if item == caller+'_'+'checkbox'
+          @flag = true
+        end
+      end
+    end
+    if @flag
+      return true
+    else
+      return false
+    end
+  end
 end
