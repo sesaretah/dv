b6d7f6544a4e0872181f5c0fbad02ebddfbc8273
Hossein Shafiei <h.shafiei@ut.ac.ir>
2018-11-09 12:42:10 +0330

    1

--- db/migrate/20181025103628_add_editable_to_workflow_state.rb
+++ db/migrate/20181025103628_add_editable_to_workflow_state.rb
@@ -1,6 +1,6 @@
 class AddEditableToWorkflowState < ActiveRecord::Migration
   def change
-    add_column :workflow_states, :editable, :integer
+    add_column :workflow_states, :editable, :text
     add_column :workflow_states, :refundable, :integer
     add_column :workflow_states, :commentable, :integer
     add_column :workflow_states, :start_point, :integer
