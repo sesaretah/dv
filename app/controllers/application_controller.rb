8a7f5d105164abb0271f3dc7a573cc3aa5468539
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-06-17 11:57:55 +0430

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
  def configure_permitted_parameters
+   update_attrs = [:password, :password_confirmation, :current_password]
+   devise_parameter_sanitizer.permit :account_update, keys: update_attrs
  end
 
  def after_sign_in_path_for(user)
