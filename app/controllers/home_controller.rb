344b6268e73dfd42cb1bcd2de50812097e407a2c
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-07-21 14:41:46 +0430

    1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -139,11 +139,11 @@ class HomeController < ApplicationController
   end
 
   def fix_query
-  #  if !params[:q].blank?
-  #    @query = "#{params[:q]} | #{params[:q].gsub('ی','ي')} | #{params[:q].gsub('ي','ی')} | #{params[:q].gsub('ک', 'ك')} | #{params[:q].gsub('ك', 'ک')} "
-  #  else
-  #    @query = ''
-  #  end
-  @query = params[:q]
+    if !params[:q].blank?
+      @query = "#{params[:q]} | #{params[:q].gsub('ی','ي').gsub('ک', 'ك')} | #{params[:q].gsub('ي','ی').gsub('ك', 'ک')}"
+    else
+      @query = ''
+    end
+  #@query = params[:q]
   end
 end
