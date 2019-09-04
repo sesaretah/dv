f89a8a04b3315ed7c50d17362126a656b185c809
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-09-04 17:23:17 +0430

    1

--- lib/scrubber.rb
+++ lib/scrubber.rb
@@ -12,13 +12,13 @@ class Scrubber
   private
     def scrub(env)
 
-      #return unless env["CONTENT_TYPE"] == "application/x-www-form-urlencoded"
+      return unless env["CONTENT_TYPE"] == "application/x-www-form-urlencoded"
       rack_input = env["rack.input"].read
       params = Rack::Utils.parse_query(rack_input, "&")
       params.each do |key, value|
       if key != 'authenticity_token'
-         c = Open3.capture3("echo #{value} | sed -e 's/[ﺀﺁﺂﺃﺄﺅﺆﺇﺈﺉﺊﺋﺌﺍﺎ]/ا/g;' -e 's/[ﺏﺐﺑﺒ]/ب/g;' -e 's/[ﺓﺔ]/ه/g;' -e 's/[ﺕﺖﺗﺘ]/ت/g;' -e 's/[ﺙﺚﺛﺜ]/ث/g;' -e 's/[ﺝﺞﺟﺠ]/ج/g;' -e 's/[ﺡﺢﺣﺤ]/ح/g;' -e 's/[ﺥﺦﺧﺨ]/خ/g;' -e 's/[ﺩﺪ]/د/g;' -e 's/[ﺫﺬ]/ذ/g;' -e 's/[ﺭﺮ]/ر/g;' -e 's/[ﺯﺰ]/ز/g;' -e 's/[ﺱﺲﺳﺴ]/س/g;' -e 's/[ﺵﺶﺷﺸ]/ش/g;' -e 's/[ﺹﺺﺻﺼ]/ص/g;' -e 's/[ﺽﺾﺿﻀ]/ض/g;' -e 's/[ﻁﻂﻃﻄ]/ط/g;' -e 's/[ﻅﻆﻇﻈ]/ظ/g;' -e 's/[ﻉﻊﻋﻌ]/ع/g;' -e 's/[ﻍﻎﻏﻐ]/غ/g;' -e 's/[ﻑﻒﻓﻔ]/ف/g;' -e 's/[ﻕﻖﻗﻘ]/ق/g;' -e 's/[ﻙﻚﻛﻜ]/ك/g;' -e 's/[ﻝﻞﻟﻠ]/ل/g;' -e 's/[ﻡﻢﻣﻤ]/م/g;' -e 's/[ﻥﻦﻧﻨ]/ن/g;' -e 's/[ﻩﻪﻫﻬ]/ه/g;' -e 's/[ﻭﻮ]/و/g;' -e 's/[ﻯﻰﻱيﻲﻳﻴ]/ي/g;' -e 's/[ﻵﻶﻷﻸﻹﻺﻻﻼ]/لا/g;'")
-         params[key] = c[0].gsub('ي','ی').gsub('ك', 'ک')
+         c = Open3.capture3("echo '#{value}' | sed -e 's/[ﺀﺁﺂﺃﺄﺅﺆﺇﺈﺉﺊﺋﺌﺍﺎ]/ا/g;' -e 's/[ﺏﺐﺑﺒ]/ب/g;' -e 's/[ﺓﺔ]/ه/g;' -e 's/[ﺕﺖﺗﺘ]/ت/g;' -e 's/[ﺙﺚﺛﺜ]/ث/g;' -e 's/[ﺝﺞﺟﺠ]/ج/g;' -e 's/[ﺡﺢﺣﺤ]/ح/g;' -e 's/[ﺥﺦﺧﺨ]/خ/g;' -e 's/[ﺩﺪ]/د/g;' -e 's/[ﺫﺬ]/ذ/g;' -e 's/[ﺭﺮ]/ر/g;' -e 's/[ﺯﺰ]/ز/g;' -e 's/[ﺱﺲﺳﺴ]/س/g;' -e 's/[ﺵﺶﺷﺸ]/ش/g;' -e 's/[ﺹﺺﺻﺼ]/ص/g;' -e 's/[ﺽﺾﺿﻀ]/ض/g;' -e 's/[ﻁﻂﻃﻄ]/ط/g;' -e 's/[ﻅﻆﻇﻈ]/ظ/g;' -e 's/[ﻉﻊﻋﻌ]/ع/g;' -e 's/[ﻍﻎﻏﻐ]/غ/g;' -e 's/[ﻑﻒﻓﻔ]/ف/g;' -e 's/[ﻕﻖﻗﻘ]/ق/g;' -e 's/[ﻙﻚﻛﻜ]/ك/g;' -e 's/[ﻝﻞﻟﻠ]/ل/g;' -e 's/[ﻡﻢﻣﻤ]/م/g;' -e 's/[ﻥﻦﻧﻨ]/ن/g;' -e 's/[ﻩﻪﻫﻬ]/ه/g;' -e 's/[ﻭﻮ]/و/g;' -e 's/[ﻯﻰﻱيﻲﻳﻴ]/ي/g;' -e 's/[ﻵﻶﻷﻸﻹﻺﻻﻼ]/لا/g;'")
+         params[key] = c[0]#.gsub('ي','ی').gsub('ك', 'ک')
       end
       end
       env["rack.input"] = StringIO.new(Rack::Utils.build_query(params))
