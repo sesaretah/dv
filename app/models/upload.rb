133aa1c52c7311ef5a7e136be30cabbf8a7dbb65
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-10-30 11:01:03 +0330

    1

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ActiveRecord::Base
   has_attached_file :attachment, :styles => { :medium => "140x140>", :tiny => "20x20>" ,:thumb => "50x50>", :large => "600x600>"  }, :original => ["100%", :jpg], :default_url => "/assets/noimage-35-:style.jpg"
-  validates_attachment_content_type :attachment, :content_type => ["image/jpg", "image/jpeg", "image/png", 'image/tiff', "application/pdf","application/vnd.ms-excel",
+  validates_attachment_content_type :attachment, :content_type => ["image/jpg", "image/jpeg", "image/png", 'image/tiff', "image/gif", "application/pdf","application/vnd.ms-excel",
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     "application/msword",
     "application/vnd.openxmlformats-officedocument.presentationml.presentation",
