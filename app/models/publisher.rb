f5457d9a405d225312ff829285241e8a76c502cf
sesaretah <hs.shafiei@gmail.com>
2020-07-29 16:12:06 +0430

    1

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,5 @@
 class Publisher < ActiveRecord::Base
+  after_save ThinkingSphinx::RealTime.callback_for(:publisher)
   has_many :articles, :through => :publications
   has_many :publications, dependent: :destroy
   has_many :publish_sources, dependent: :destroy
