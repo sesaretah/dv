96128b8e4a94c6bb98deb284c330e5ec71af8e4b
sesaretah <hs.shafiei@gmail.com>
2020-09-09 23:35:46 +0430

    1

--- db/migrate/20200905053646_add_article_id_indexes.rb
+++ db/migrate/20200905053646_add_article_id_indexes.rb
@@ -1,17 +1,17 @@
 class AddArticleIdIndexes < ActiveRecord::Migration
   def change
-     add_index :areaings, :article_id
-     add_index :contributions, :article_id
-     add_index :datings, :article_id
-     add_index :formatings, :article_id
-     add_index :involvements, :article_id
-     add_index :kinships, :article_id
-     add_index :originatings, :article_id
-     add_index :publications, :article_id
-     add_index :speakings, :article_id
+    # add_index :areaings, :article_id
+    # add_index :contributions, :article_id
+    # add_index :datings, :article_id
+    # add_index :formatings, :article_id
+    # add_index :involvements, :article_id
+    # add_index :kinships, :article_id
+    # add_index :originatings, :article_id
+    # add_index :publications, :article_id
+    # add_index :speakings, :article_id
     
-     add_index :titlings, :article_id
-     add_index :typings, :article_id
-     add_index :workflow_transitions, :article_id
+    # add_index :titlings, :article_id
+    # add_index :typings, :article_id
+    # add_index :workflow_transitions, :article_id
   end
 end
