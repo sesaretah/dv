92727a5a9cdce3e0e7bb0c74b08f29d895c43483
sesaretah <hs.shafiei@gmail.com>
2020-10-25 23:50:33 +0330

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -106,6 +106,7 @@ class ApplicationController < ActionController::Base
 
   def article_owner(article, user)
     return true if article.user_id = user.id
+    return true if grant_access('edit_workflow', user)
     @role_workflow_state_ids = WorkflowState.where(role_id: user.current_role_id).pluck(:id).uniq
     @user_workflows = user.workflows.pluck(:id)
     if @role_workflow_state_ids.include?(article.workflow_state.id) || @user_workflows.include?(article.workflow_state.workflow.id)
