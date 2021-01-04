bd351b49da8f9643658a643f508b948c3e466ee4
sesaretah <hs.shafiei@gmail.com>
2021-01-04 16:43:05 +0330

    1

--- app/controllers/role_accesses_controller.rb
+++ app/controllers/role_accesses_controller.rb
@@ -1,11 +1,12 @@
 class RoleAccessesController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+
   def create
     @role_access = RoleAccess.create(role_id: params[:role_id], access_group_id: params[:access_group_id])
   end
 
   def destroy
-    @role_access = RoleAccess.where(role_id: params[:role_id], access_group_id: params[:access_group_id])
+    @role_access = RoleAccess.where(role_id: params[:role_id], access_group_id: params[:access_group_id]).first
     if !@role_access.blank?
       @role_access.destroy
     end
