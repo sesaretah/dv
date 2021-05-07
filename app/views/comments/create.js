d530e8b28b8955f2403de863bcd1d3ec41a201da
sesaretah <hs.shafiei@gmail.com>
2021-05-07 15:19:47 +0430

    1

--- app/views/comments/create.js
+++ app/views/comments/create.js
@@ -1,4 +1,9 @@
+<% if @comment.comment_type == 'vote'%>
+$("#vote-comments").replaceWith("<%= escape_javascript(render(:partial => 'comments/list', locals: {comments: @article.comments.where(comment_type: 'vote').order('created_at desc') })) %>");
+$('#comment_content').val('');
+<%else%>
 $("#article-show").replaceWith("<%= escape_javascript(render(:partial => 'articles/article_comments', locals: {article: @article, nxt: @next_workflow_states, prv: @previous_workflow_states})) %>");
+<%end%>
 new Noty({
     type: 'success',
     theme    : 'relax',
