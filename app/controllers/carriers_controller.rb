77f4ace0499fb19e60dbdbee65f570b95346f45d
sesaretah <hs.shafiei@gmail.com>
2021-05-07 18:16:51 +0430

    1

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,51 +1,48 @@
 class CarriersController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-    def new
-        @workflow = Workflow.find(params[:workflow_id])
-        if !owner(@workflow, current_user) && !grant_access('edit_workflow', current_user)
-            head(403)
-        end
-       
+  def new
+    @workflow = Workflow.find(params[:workflow_id])
+    if !owner(@workflow, current_user) && !grant_access("edit_workflow", current_user)
+      head(403)
     end
+  end
 
+  def carry
+    @carrier = Carrier.first
+    CarrierWorker.perform_async(params[:id])
+  end
 
-    def carry
-        p '%%%%%%%%%%%%'
-        @carrier = Carrier.first
-        CarrierWorker.perform_async(params[:id])
-    end
-    
-    def show
-        @carrier = Carrier.find(params[:id])
-    end
-    
-    def index
-        @workflow = Workflow.find(params[:workflow_id])
-        @carriers = Carrier.where('source_workflow_state_id in (?)', @workflow.workflow_states.pluck(:id).uniq).order('source_workflow_state_id asc')
-    end
-    
-    def create
-        carrier = Carrier.where(source_workflow_state_id: params[:carrier][:source_workflow_state_id]).first
-        if carrier.blank?
-            carrier = Carrier.create(carrier_params)
-        else
-            carrier.update(carrier_params)
-        end
-        @workflow = carrier.source_state.workflow
-        @carriers = Carrier.where('source_workflow_state_id in (?)', @workflow.workflow_states.pluck(:id).uniq).order('source_workflow_state_id asc')
-    end
-  
-    def destroy
-      @carrier = Carrier.find(params[:id])
-      @workflow = @carrier.source_state.workflow
-      @carriers = Carrier.where('source_workflow_state_id in (?)', @workflow.workflow_states.pluck(:id).uniq).order('source_workflow_state_id asc')
-      if !@carrier.blank?
-        @carrier.destroy
-      end
-    end
+  def show
+    @carrier = Carrier.find(params[:id])
+  end
 
-    def carrier_params
-        params.require(:carrier).permit(:user_id,:source_workflow_state_id, :target_workflow_state_id, :trials, :voting_condition, :timer)
+  def index
+    @workflow = Workflow.find(params[:workflow_id])
+    @carriers = Carrier.where("source_workflow_state_id in (?)", @workflow.workflow_states.pluck(:id).uniq).order("source_workflow_state_id asc")
+  end
+
+  def create
+    carrier = Carrier.where(source_workflow_state_id: params[:carrier][:source_workflow_state_id]).first
+    if carrier.blank?
+      carrier = Carrier.create(carrier_params)
+    else
+      carrier.update(carrier_params)
     end
+    @workflow = carrier.source_state.workflow
+    @carriers = Carrier.where("source_workflow_state_id in (?)", @workflow.workflow_states.pluck(:id).uniq).order("source_workflow_state_id asc")
+  end
+
+  def destroy
+    @carrier = Carrier.find(params[:id])
+    @workflow = @carrier.source_state.workflow
+    @carriers = Carrier.where("source_workflow_state_id in (?)", @workflow.workflow_states.pluck(:id).uniq).order("source_workflow_state_id asc")
+    if !@carrier.blank?
+      @carrier.destroy
+    end
+  end
+
+  def carrier_params
+    params.require(:carrier).permit(:user_id, :source_workflow_state_id, :target_workflow_state_id, :trials, :voting_condition, :timer)
+  end
 end
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,64 +1,63 @@
 class Carrier < ActiveRecord::Base
-    
-    def carry_articles
-        for article in self.source_state.articles
-            self.timer.blank? ? timer = 0 : timer = self.timer
-            if !article.workflow_transitions.blank? 
-                transition = article.workflow_transitions.last.created_at
-            else
-                transition = article.created_at
-            end
-            vote_flag = true
-            if self.source_state.is_votable
-                case self.voting_condition
-                when 0
-                    vote_flag = true
-                when 1
-                    self.source_state.majority ? vote_flag = true : vote_flag=false
-                when 2
-                    self.source_state.consensus ? vote_flag = true : vote_flag=false
-                end
-            end
-            if (Time.now > transition + timer.minutes && vote_flag)
-                article.workflow_state_id = self.target_state.id
-                article.save
-                self.done = true
-                self.save
-            end
+  def carry_articles
+    for article in self.source_state.articles
+      self.timer.blank? ? timer = 0 : timer = self.timer
+      if !article.workflow_transitions.blank?
+        transition = article.workflow_transitions.last.created_at
+      else
+        transition = article.created_at
+      end
+      vote_flag = true
+      if self.source_state.is_votable
+        case self.voting_condition
+        when 0
+          vote_flag = true
+        when 1
+          self.source_state.majority ? vote_flag = true : vote_flag = false
+        when 2
+          self.source_state.consensus ? vote_flag = true : vote_flag = false
         end
-    end
-
-    def reduce_trial
-        if self.trials > 0
-            self.trials -= 1
-        end
+      end
+      if (Time.now > transition + timer.hours && vote_flag)
+        article.workflow_state_id = self.target_state.id
+        article.save
+        self.done = true
         self.save
+      end
     end
+  end
 
-    def choose_rule
-        rule = self.extract_rules
-        self.timer_rule if rule['type'] == 'timer' && !self.done
+  def reduce_trial
+    if self.trials > 0
+      self.trials -= 1
     end
+    self.save
+  end
 
-    def extract_rule
-        result = {}
-        if !self.rules.blank?
-            result = JSON.parse(self.rules)
-        end
-        return result
-    end
+  def choose_rule
+    rule = self.extract_rules
+    self.timer_rule if rule["type"] == "timer" && !self.done
+  end
 
-    def self.carry(r)
-        for carrier in self.all
-            carrier.carry_articles
-        end
+  def extract_rule
+    result = {}
+    if !self.rules.blank?
+      result = JSON.parse(self.rules)
     end
+    return result
+  end
 
-    def source_state
-        WorkflowState.find(self.source_workflow_state_id)
+  def self.carry(r)
+    for carrier in self.all
+      carrier.carry_articles
     end
+  end
 
-    def target_state
-        WorkflowState.find(self.target_workflow_state_id)
-    end
+  def source_state
+    WorkflowState.find(self.source_workflow_state_id)
+  end
+
+  def target_state
+    WorkflowState.find(self.target_workflow_state_id)
+  end
 end
