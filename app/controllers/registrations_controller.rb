829bd290b7e04e0d13173e5b286718e89baf8fa8
Hossein Shafiei <h.shafiei@ut.ac.ir>
2022-08-28 10:41:46 +0430

    added more logs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -49,7 +49,7 @@ class RegistrationsController < Devise::RegistrationsController
     @result = Hash.from_xml(response.gsub("\n", ""))
     if !@result['serviceResponse']['authenticationSuccess'].blank?
       @utid = @result['serviceResponse']['authenticationSuccess']['user']
-      Rails.logger.info @result['serviceResponse']
+      Rails.logger.info @result['serviceResponse']['authenticationSuccess']['attributes']
       @sso = Sso.where(utid: @utid).first
       if @sso.blank?
         @sso = Sso.create(utid: @utid, uuid: SecureRandom.uuid)
