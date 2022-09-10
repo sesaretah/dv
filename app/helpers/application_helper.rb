fe6dc488bb4d63db0842b6416c84e34037db3a79
Hossein Shafiei <h.shafiei@ut.ac.ir>
2022-09-10 10:44:38 +0200

    Adds article_owner to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -205,13 +205,35 @@ module ApplicationHelper
     end
   end
 
-  def article_owner(article, user)
+  # def article_owner(article, user)
+  #   @role_workflow_state_ids = WorkflowState.where(role_id: user.current_role_id).pluck(:id).uniq
+  #   @user_workflows = user.workflows.pluck(:id)
+  #   if !article.workflow_state.blank? && (@role_workflow_state_ids.include?(article.workflow_state.id) || @user_workflows.include?(article.workflow_state.workflow.id))
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  def article_owner(article, user, action='edit')
+    return true if article.user_id = user.id
+    return true if grant_access("edit_workflow", user)
     @role_workflow_state_ids = WorkflowState.where(role_id: user.current_role_id).pluck(:id).uniq
     @user_workflows = user.workflows.pluck(:id)
-    if !article.workflow_state.blank? && (@role_workflow_state_ids.include?(article.workflow_state.id) || @user_workflows.include?(article.workflow_state.workflow.id))
-      return true
-    else
-      return false
+    if action == 'edit'
+      if @role_workflow_state_ids.include?(article.workflow_state.id) || @user_workflows.include?(article.workflow_state.workflow.id)
+        return true
+      else
+        return false
+      end
+    end
+
+    if action == 'destory'
+      if @user_workflows.include?(article.workflow_state.workflow.id)
+        return true
+      else
+        return false
+      end
     end
   end
 end
