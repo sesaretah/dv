ef70d88a1b5d311308f8141e65fc85cd02563aa1
Hossein Shafiei <hs.shafiei@gmail.com>
2023-02-12 13:34:52 +0100

    adds tag

--- app/models/access_grouping.rb
+++ app/models/access_grouping.rb
@@ -4,10 +4,6 @@ class AccessGrouping < ActiveRecord::Base
   belongs_to :article
 
   def generate_notification
-    body = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"> <soap12:Body> <Insert_Document xmlns=\"http://tempuri.org/\"> <UserName>tehranuni</UserName> <Password>98765432100</Password> <ownerDepartmentId>4BE6D74C-56A7-4871-B13F-51D844D5DC51</ownerDepartmentId> <ownerPersonnelId>3099FF0E-B897-4FB4-A0BF-507ACAABE1D0</ownerPersonnelId> <Subject>#{article.title}</Subject> <Description>#{ActionView::Base.full_sanitizer.sanitize(article.content)}</Description> <docDate></docDate> <keyword></keyword> <SecurityId>Normal</SecurityId> <UrgencyId>Normal</UrgencyId> <LetterType>Taypi</LetterType> <ErrStatus>0</ErrStatus> <ErrMessage>-</ErrMessage> <ErrFunctionName>-</ErrFunctionName> </Insert_Document> </soap12:Body> </soap12:Envelope>"
-    HTTParty.post('http://192.168.112.185/IstgInternalDocWebService/InternalDocuments.asmx', body: body,
-                                                                                             headers: { 'Content-Type' => 'text/xml; charset=utf-8' })
-
     return unless notify
 
     # Notification.create(user_id: self.user_id, notifiable_type: "Article", notifiable_id: self.article_id, notification_type: "access_grouping", emmiter_id: self.user_id)
@@ -23,7 +19,7 @@ class AccessGrouping < ActiveRecord::Base
       # if prev.blank?
       user = User.find(uniq_user_id)
       if uniq_user_id == user_id && !user.profile.blank? && !user.profile.dabir_department_id.blank? && !user.profile.dabir_personnel_id.blank?
-        body = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"> <soap12:Body> <Insert_Document xmlns=\"http://tempuri.org/\"> <UserName>tehranuni</UserName> <Password>98765432100</Password> <ownerDepartmentId>#{user.profile.dabir_department_id}</ownerDepartmentId> <ownerPersonnelId>#{user.profile.dabir_personnel_id}</ownerPersonnelId> <Subject><![CDATA[#{article.title}]]</Subject> <Description><![CDATA[#{article.textual}]]</Description> <docDate></docDate> <keyword></keyword> <SecurityId>Normal</SecurityId> <UrgencyId>Normal</UrgencyId> <LetterType>Taypi</LetterType> <ErrStatus>0</ErrStatus> <ErrMessage>-</ErrMessage> <ErrFunctionName>-</ErrFunctionName> </Insert_Document> </soap12:Body> </soap12:Envelope>"
+        body = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"> <soap12:Body> <Insert_Document xmlns=\"http://tempuri.org/\"> <UserName>tehranuni</UserName> <Password>98765432100</Password> <ownerDepartmentId>#{user.profile.dabir_department_id}</ownerDepartmentId> <ownerPersonnelId>#{user.profile.dabir_personnel_id}</ownerPersonnelId> <Subject>#{article.title}</Subject> <Description> <![CDATA[ <html><body>#{article.textual}</html></body> ]]> </Description> <docDate></docDate> <keyword></keyword> <SecurityId>Normal</SecurityId> <UrgencyId>Normal</UrgencyId> <LetterType>Taypi</LetterType> <ErrStatus>0</ErrStatus> <ErrMessage>-</ErrMessage> <ErrFunctionName>-</ErrFunctionName> </Insert_Document> </soap12:Body> </soap12:Envelope>"
         HTTParty.post('http://192.168.112.185/IstgInternalDocWebService/InternalDocuments.asmx', body: body,
                                                                                                  headers: { 'Content-Type' => 'text/xml; charset=utf-8' })
 
