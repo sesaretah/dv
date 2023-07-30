0f6e3fedd75fd1ff1a94c61bbf2b1df704bc4790
Hossein Shafiei <hossein.shafiei@new-work.se>
2023-07-30 12:55:47 +0330

    Update pdfs_worker.rb

--- app/workers/pdfs_worker.rb
+++ app/workers/pdfs_worker.rb
@@ -11,7 +11,7 @@ class PdfsWorker
       system("wkhtmltopdf --page-size Letter --viewport-size 1280x1024  --margin-right 15mm --margin-bottom 20mm #{Rails.application.routes.default_url_options[:host]}/articles/#{type}/#{id} #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf")
       for upload in Upload.where("uploadable_type = ? AND uploadable_id =  ? AND attachment_type IN  (?) AND printable is true", "Article", id, ["article_citation", "article_attachment", "article_documents"])
         if upload.attachment_content_type == "application/pdf"
-          system("convert -density 150 #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf #{upload.attachment.path} #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf")
+          system("convert -density 600 #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf #{upload.attachment.path} -resize 50% #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf")
         end
       end
       for kinship in article.kinships
@@ -29,7 +29,7 @@ class PdfsWorker
         end
         #p "#{kin_uploads}"
         if kin_uploads != " "
-          system("convert -density 150 #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf #{kin_uploads} #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf")
+          system("convert -density 600 #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf #{kin_uploads} -resize 50% #{Rails.root}/public/pdfs/#{id}/#{uuid}.pdf")
         end
       end
       article.pdf_generated = true
