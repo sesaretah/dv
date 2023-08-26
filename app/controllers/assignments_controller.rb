41532d623ec9388df7bb782d34d64b68e4f285e7
Hossein Shafiei <hs.shafiei@gmail.com>
2023-08-26 11:06:17 +0200

    bug

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -66,13 +66,14 @@ class AssignmentsController < ApplicationController
   def destroy
     if !grant_access("alter_assignments", current_user) || @assignment.assigner_id != current_user.id
       head(403)
-    end
+    else 
     send_mail user_id: @assignment.user_id, role_title: @assignment.role.title, mail_type: 'role_unassignment'
     @assignment.destroy
     respond_to do |format|
       format.html { redirect_to assignments_url, notice: 'Assignment was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
   end
 
   private
