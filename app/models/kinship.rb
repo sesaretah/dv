3a917153f60e03c58c13dbe05ae705b860ecd464
Hossein Shafiei <hs.shafiei@gmail.com>
2023-10-21 11:22:58 +0200

    1

--- app/models/kinship.rb
+++ app/models/kinship.rb
@@ -8,12 +8,22 @@ class Kinship < ActiveRecord::Base
 
   after_save :create_reverse
 
+  after_destroy :destroy_reverse 
+
   def create_reverse
     if !article_relation_type.reverse_relation_id.blank?
-      reverse_kinship = Kinship.where(kin: article_id, article_relation_type_id: article_relation_type.reverse_relation_id).first
+      reverse_kinship = Kinship.where(kin_id: article_id, article_id: kin.id, article_relation_type_id: article_relation_type.reverse_relation_id).first
       if !reverse_kinship.present?
         Kinship.create(kin_id: article_id, article_id: kin.id, article_relation_type_id: article_relation_type.reverse_relation_id)
       end
     end
   end
+
+  def destroy_reverse
+    reverse_kinship = Kinship.where(kin_id: article_id, article_id: kin.id, article_relation_type_id: article_relation_type.reverse_relation_id).first
+    if reverse_kinship.present?
+      reverse_kinship.destroy
+    end
+  end 
+
 end
