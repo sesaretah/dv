b774d4df03b730c6365eab83272c865597a104f1
Hossein Shafiei <hs.shafiei@gmail.com>
2023-10-21 11:02:07 +0200

    adds reverse kin

--- app/models/kinship.rb
+++ app/models/kinship.rb
@@ -5,4 +5,15 @@ class Kinship < ActiveRecord::Base
   belongs_to :article
   belongs_to :kin, :class_name => "Article"
   belongs_to :article_relation_type
+
+  after_save :create_reverse
+
+  def create_reverse
+    if !article_relation_type.reverse_relation_id.blank?
+      reverse_kinship = Kinship.where(kin: article_id, reverse_relation_id: reverse_relation_id).first
+      if !reverse_kinship.present?
+        Kinship.create(kin: article_id, article_id: kin.id, reverse_relation_id: reverse_relation_id)
+      end
+    end
+  end
 end
