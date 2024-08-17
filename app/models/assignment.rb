af49a0626ee80fbce1ff5e9ffc5b9abdb60e7b55
Hossein Shafiei <hs.shafiei@gmail.com>
2024-08-17 11:18:07 +0200

    adds before destroy

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,6 +6,8 @@ class Assignment < ActiveRecord::Base
   belongs_to :role
 
   before_save :check_duplicate
+  
+  before_destroy :update_home_setting
 
   def check_duplicate
     assigned = Assignment.where(user_id: self.user_id, role_id: self.role_id).first
@@ -13,6 +15,18 @@ class Assignment < ActiveRecord::Base
       false
     else
       true
+    end
+  end
+
+  def update_home_setting
+    begin  
+      workflow_state_id = user.home_setting.workflow_state
+      workflow_state = WorkflowState.find_by(id: workflow_state_id)
+      if workflow_state.role_id == role.id
+        user.home_setting.update(workflow_state: -1)
+      end
+    rescue StandError
+      nil 
     end
   end
 
